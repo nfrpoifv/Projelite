7f5d5f14ac5257a86b773fcba362355e979bb191
nfrpoifv <danielaraya039@gmail.com>
2025-12-07 12:15:04 -0600

    All the models and changes to the project's folder structure to bring more order

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
+
+
+const db = require('./config/database'); 
 
 app.get('/', (req, res) => {
-  res.send('IT WORKS');
+ 
+  db.getConnection((err, connection) => {
+    if (err) {
+      console.error(' Error al obtener conexión:', err.message);
+      res.send('Error al conectar con la base de datos');
+    } else {
+      console.log('Conexión a la base de datos exitosa desde /');
+      connection.release();
+      res.send('IT WORKS and DB connected');
+    }
+  });
 });
 
 app.listen(port, () => {
